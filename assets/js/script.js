43d6f62d895098c4c7fe1261df83fd061b832b52
randronaco <randronaco1027@gmail.com>
2022-03-15 15:53:41 -0400

    finalize 4.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,22 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
-  event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
-  var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
-  var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
-  };
-  createTaskEl(taskDataObj);
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.requestFullscreen();
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj){
+var createTaskEl = function (taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
